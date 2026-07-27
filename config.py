03db1d4d8a38cf23954f1dd2db368521af13ca2e
ullsesojeda <ulisesojeda@msn.com>
2026-07-27 17:12:52 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+# Base de datos local o en Render
+if os.path.exists("/montesion/gastos"):
+    DATABASE_PATH = "/montesion/gastos/gastos.db"
+else:
+    DATABASE_PATH = os.path.join(BASE_DIR, "database", "gastos.db")
+
 class Config:
-    SECRET_KEY = 'cambia_esta_clave_super_secreta'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-        BASE_DIR,
-        'database',
-        'gastos.db'
-    )
+    SECRET_KEY = "cambia_esta_clave_super_secreta"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
